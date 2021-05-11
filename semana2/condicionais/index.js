22cdec66a02485dd24f075215a6298b01eb4b7f8
Carlos Felipe <carlos.felipe@hotmail.com.br>
2021-05-11 13:29:16 -0300

    interpretacao

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,8 @@
+const numero = Number(prompt("Digite o primeiro número."))
+
+if(numero > 0) {
+  console.log("Esse número passou no teste")
+	let mensagem = "Essa mensagem é secreta!!!"
+}
+
+console.log(mensagem)
