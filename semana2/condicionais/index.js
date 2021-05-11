b7cf6509ef1fc6fd85bb171b785096ae844f8825
Carlos Felipe <carlos.felipe@hotmail.com.br>
2021-05-11 13:44:25 -0300

    escrita feito

--- semana2/condicionais/index.js
+++ semana2/condicionais/index.js
@@ -1,8 +1,42 @@
-const numero = Number(prompt("Digite o primeiro número."))
+const habilitado = Number(prompt("Qual a sua idade?"))
 
-if(numero > 0) {
-  console.log("Esse número passou no teste")
-	let mensagem = "Essa mensagem é secreta!!!"
-}
+if(habilitado > 18) {
+  console.log("Você pode dirigir")
+	 } else console.log("Você nao pode dirigir")
 
-console.log(mensagem)
+
+     const turno = prompt("Qual o seu turno?")
+
+
+     // exercicio 2
+
+
+    //  if(turno ===`M`){
+    //      console.log("Bom dia!")
+    //  } else if(turno === 'V'){
+    //      console.log('Boa tarde!')
+    //  } else if(turno === 'N'){
+    //      console.log('Boa Noite!')
+    //  }
+
+    switch (turno) {
+        case "M":
+            console.log("Bom dia!")
+            break;
+            case "V":
+                console.log('Boa tarde!')
+            break;
+            case "N":
+                console.log('Boa Noite!')
+
+    }
+
+// Exercicio 3
+
+
+    const genero = prompt("Qual o Genero do filme?")
+    const valor = Number(prompt("Quanto custa o ingresso"))
+
+    if(genero == 'fantasia' && valor < 15){
+        console.log(`bom filme `)
+    } else console.log('Escolha outro filme :(')
