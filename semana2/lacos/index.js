e7832ea3eb4e6fcbe220ff7a19a8d4a9f1ad2687
Carlos Felipe <carlos.felipe@hotmail.com.br>
2021-05-12 13:12:35 -0300

    exercicio 3

--- semana2/lacos/index.js
+++ semana2/lacos/index.js
@@ -11,3 +11,66 @@ bichos.push(prompt("digite o nome do seu pet"))
 }
 
 console.log(bichos)
+
+
+//exercicio 2 
+
+///a)
+
+function imprimeValor(array){
+for(let i=0; i<=array.length-1;i++){
+console.log(array[i])
+
+}
+
+}
+
+// b 
+
+function imprimeValorDivididoPorDez(array){
+    for(let i=0; i<=array.length-1;i++){
+    console.log(array[i]/10)
+    
+    }
+    
+    }
+
+
+    //c 
+
+    function imprimeValorPar(array){
+        let novoArray = []
+        for(let i=0; i<=array.length-1;i++){
+            if(array[i]%2 === 0){
+        novoArray.push(array[i])
+            }
+        }
+        console.log(novoArray)
+        }
+
+//d
+
+
+function imprimeIndexEValor(array){
+    for(let i=0; i<=array.length-1;i++){
+    console.log(`O elemento do índex ${i} é: ${array[i]}`)
+    
+    }
+    
+    }
+    
+    //e
+
+    function maiorEMenor(array){
+let maior = array[0] 
+let menor = array[0]
+for(let i=0; i<=array.length-1;i++){
+if(array[i]> maior ){
+    maior = array[i]
+}
+if(array[i] < menor){
+    menor = array[i]
+}
+}
+console.log(`"O maior número é ${maior} e o menor é ${menor}`)
+    }
