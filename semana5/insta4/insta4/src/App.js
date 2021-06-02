b7a2873f9e308a7b7a9661968e477cd60a8a54be
carl0sfelipe <carlos.felipe@hotmail.com.br>
2021-06-02 20:01:11 -0300

    labegram feito

---
+++ semana5/insta4/insta4/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+<Post
+          nomeUsuario={'joao.97'}
+          fotoUsuario={'https://picsum.photos/50/49'}
+          fotoPost={'https://picsum.photos/200/120'}
+        />
+
+<Post
+          nomeUsuario={'pedro.se'}
+          fotoUsuario={'https://picsum.photos/50/47'}
+          fotoPost={'https://picsum.photos/200/110'}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
