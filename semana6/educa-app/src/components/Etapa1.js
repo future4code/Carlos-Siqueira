c39507df4af385b2b17a0992261f0372255963af
carl0sfelipe <carlos.felipe@hotmail.com.br>
2021-06-07 20:05:06 -0300

    formulario basico

---
+++ semana6/educa-app/src/components/Etapa1.js
@@ -0,0 +1,25 @@
+function Etapa1() {
+    return (
+      <div >
+      <h1> Etapa 1 -  DADOS GERAIS</h1>
+      <h3>1. Qual o seu nome ?</h3>
+      <input></input>
+      <h3>2. qual a sua idade?</h3>
+      <input></input>
+      <h3>3. Qual o seu email ?</h3>
+      <input></input>
+      <h3>4. Qual a sua escolaridade ?</h3>
+
+  <select id="form" name="form">
+    <option value="medio">Endino medio completo</option>
+    <option value="supperior-incompleto">Superior incompleto</option>
+    <option value="superior-completo">Superior completo</option>
+    <option value="mestrado">Mestrado</option>
+  </select>
+  
+      </div>
+    );
+  }
+  
+  export default Etapa1;
+  
---
+++ semana6/educa-app/src/components/Etapa3.js
@@ -0,0 +1,15 @@
+function Etapa3() {
+    return (
+      <div >
+      <h1> Etapa 3 -  INFORMAÇOES GERAIS DE ENSINO</h1>
+      <h3>1. Porque voce nao terminou um curso de graduaçao?</h3>
+      <input></input>
+      <h3>2. Voce fez algum curso complementar?</h3>
+      <input></input>
+      
+      </div>
+    );
+  }
+  
+  export default Etapa3;
+  
